85699ce279f6329bc2422a534d782451686deb12
Chris Kedzie <kedzie@cs.columbia.edu>
2016-09-27 13:26:51 -0700

    Updated test runner.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,4 +1,11 @@
-th test.PriorityQueueSimpleEncoderCuda.lua
-th test.PriorityQueueSimpleDecoderCuda.lua
-th test.PriorityQueueSimpleEncoder.lua
+th test.SortOnKey.lua
+th test.BilinearAttentionMemoryWriter.lua
+th test.LinearMemoryWriter.lua
+th test.MemoryCell.lua
+
+
+#th test.PriorityQueueSimpleEncoder.lua
+#th test.PriorityQueueSimpleAttentionEncoder.lua
 th test.PriorityQueueSimpleDecoder.lua
+#th test.PriorityQueueSimpleEncoderCuda.lua
+th test.PriorityQueueSimpleDecoderCuda.lua
