57d006c6774a5a9bb57c17ec60ac94022bc1ded7
Chris Kedzie <kedzie@cs.columbia.edu>
2016-09-28 11:40:26 -0700

    Training scripts simplified.

--- sbin/lstm-toy-model.lua
+++ sbin/lstm-toy-model.lua
@@ -2,18 +2,18 @@
 local opt = lapp [[
 Train a chunked sentence LSTM autoencoder.
 Options:
-   --input-vocab    (string)        token vocab path 
-   --output-vocab    (string)        token vocab path 
-   --train-path     (string)        train path 
-   --test-path      (string)        test path 
-   --test-path-large      (string)        test path 
-   --batch-size     (default 5)     batch size
-   --learning-rate (default .001) learning rate
-   --dim-size       (default 200)   word vector and hidden unit dimensionality
-   --max-epochs     (default 20) max number of epochs to run for
-   --seed           (default 1)     seed for torch random number generator
-   --progress-bar    show progress bar
-   --gpu (default 0) which gpu to use. 0 means cpu
+   --input-vocab     (string)        Encoder vocab path 
+   --output-vocab    (string)        Decoder vocab path 
+   --train-path      (string)        Training data path
+   --test-path       (string)        Test data path (same avg. length as train)
+   --test-path-large (string)        Test data path (larger length than train) 
+   --batch-size      (default 50)    Batch size
+   --learning-rate   (default .001)  Learning rate
+   --dim-size        (default 200)   Dimension of of all embeddings, units.
+   --num-layers      (default 2)     Number of stacked lstms.
+   --seed            (default 1)     Seed for torch random number generator
+   --progress-bar                    Show progress bar
+   --gpu             (default 0)     Which gpu to use. Default is cpu.
 ]]
 
 -- require torch packages
@@ -40,19 +40,20 @@ end
 -- require my packages
 require 'data'
 require 'memory'
-require 'baseline'
+require 'models'
+
 
 local inputVocab = data.readVocab(opt.input_vocab, false)
 local outputVocab = data.readVocab(opt.output_vocab, true)
 
-local trainData = data.readData(opt.train_path, inputVocab, outputVocab, false)
-local testData = data.readData(opt.test_path, inputVocab, outputVocab, false)
+local trainData = data.readData(opt.train_path, inputVocab, outputVocab, true)
+local testData = data.readData(opt.test_path, inputVocab, outputVocab, true)
 local testDataLarge = data.readData(
-    opt.test_path_large, inputVocab, outputVocab, false)
+    opt.test_path_large, inputVocab, outputVocab, true)
 
 optimState = {learningRate=opt.learning_rate}
-local model = nn.CoupledLSTM(inputVocab.size, outputVocab.size, 
-    opt.dim_size, optimState) 
+local model = nn.memory.LSTMTransducer(inputVocab.size, outputVocab.size, 
+    opt.dim_size, opt.num_layers, optimState) 
 
 if use_GPU then model:cuda() end
 
@@ -60,25 +61,33 @@ local trainingTimes = {}
 local testTimes = {}
 local testTimesLarge = {}
 
-for epoch=1,opt.max_epochs do
-    print("Epoch "..epoch)
+local numTrainChunks = math.floor(trainData.sizeExamples / 1000)
+print("Splitting training data into " .. numTrainChunks .. " chunks.")
+local trainDataChunks = data.chunkDataset(trainData, numTrainChunks)
+local lastTrainIndex = 1
+for chunk=1,#trainDataChunks do
+    local nextTrainIndex = lastTrainIndex + trainDataChunks[chunk].sizeExamples
+
+    print("\nTrain chunk " .. lastTrainIndex .." ..  " .. nextTrainIndex-1)
+    lastTrainIndex = nextTrainIndex
 
     -- TRAINING --
 
+    local trainChunk = trainDataChunks[chunk]
     local trainLoss = 0
     local startTrain = os.clock()
-    for batch in data.batches(trainData, opt.batch_size, use_GPU) do 
+    for batch in data.batches(trainChunk, opt.batch_size, use_GPU) do 
         if opt.progress_bar then
-            xlua.progress(batch.position, trainData.sizeExamples)
+            xlua.progress(batch.position, trainChunk.sizeExamples)
         end
         local err = model:train(
             batch.encoderInput, batch.decoderInput, batch.decoderOutput)
         trainLoss = trainLoss + err
     end
     local stopTrain = os.clock()
-    local trainPerpl = torch.exp(trainLoss / trainData.sizePredictions)
+    local trainAvgNll = trainLoss / trainChunk.sizeExamples
     print(string.format("Training time %.2f", stopTrain - startTrain))
-    print("Training perpl. = "..trainPerpl)
+    print("Training avg. nll = ".. trainAvgNll)
     table.insert(trainingTimes, stopTrain - startTrain)
 
     -- TEST (same avg. size as training set) --
@@ -129,4 +138,3 @@ print(string.format("Average training time per epoch = %.2f", avgTrainTime))
 print(string.format("Average training time per epoch = %.2f", avgTestTime))
 print(
     string.format("Average training time per epoch = %.2f", avgTestTimeLarge))
-
